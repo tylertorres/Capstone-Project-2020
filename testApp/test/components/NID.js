4dfca71f2f1bdf4b729fca05833f2c630d5aa457
Nicholas Samaroo <ndsamaroo543@gmail.com>
2020-03-30 08:39:33 -0400

    Updated NID page with rating bar, avatars, and chat button

--- testApp/test/components/NID.js
+++ testApp/test/components/NID.js
@@ -1,53 +1,240 @@
 import React, {Component} from 'react';
-import {StyleSheet, View, Text, Image, ImageBackground, TextInput, Button, StatusBar, TouchableOpacity} from 'react-native';
+import { Divider, Avatar, Rating, Button } from 'react-native-elements';
+import Icon from 'react-native-vector-icons/FontAwesome';
+//import { Rating, AirbnbRating } from 'react-native-ratings';
+import {StyleSheet, View, Text, Image, ImageBackground, TextInput,StatusBar, TouchableOpacity} from 'react-native';
 import { GoogleSignin, GoogleSigninButton, statusCodes} from 'react-native-google-signin';
 import { disableExpoCliLogging } from 'expo/build/logs/Logs';
 
 
 
 function NID({ navigation }) {
-    return (
-      <View style = {styles.mainView}>
-        <Text style = {styles.findID}> Find a Designer </Text>
-        
-        <Button style = {styles.stay}
-          title = "This is the NID page"
-          onPress={() => navigation.navigate('NID')}
-        />
+    return (    
+  
+  <View style = {styles.designers}>
+    
+    <Text style = {styles.findID}> Find a Designer </Text>
+
+    <Divider style = {styles.divider} />
+
+    <TouchableOpacity style = {{flexDirection: 'row'}}> 
+      <Avatar 
+        size = "large"
+        rounded
+        source={{ uri: 'https://s3.amazonaws.com/uifaces/faces/twitter/ladylexy/128.jpg' }} />
       
-      <View style = {styles.goBackContainer}>
-        <TouchableOpacity style = {styles.goBack} onPress={() => navigation.popToTop()}>
-          <Text style = {styles.goBackText}> GO BACK TO THE FIRST SCREEN IN THE STACK </Text>
-        </TouchableOpacity>
+      <View> 
+        <Text style = {styles.designerName}> Jane Doe</Text>
+        <Text style = {styles.businessName}> Doe Inc. </Text>
       </View>
-        
+      
+      
+      <Rating
+        type = 'custom'
+        imageSize = {35}
+        ratingColor = '#3498db'
+        ratingBackgroundColor= 'transparent'
+        readonly
+        startingValue={3}
+        style = {{justifyContent: 'center', paddingLeft: 30}}  />
+
+    </TouchableOpacity>
+
+    <Divider style = {styles.divider} />
+  
+    <TouchableOpacity style = {{flexDirection: 'row'}}> 
+      <Avatar 
+        size = "large"
+        rounded
+        source={{ uri: 'https://s3.amazonaws.com/uifaces/faces/twitter/ladylexy/128.jpg' }} />
+      
+      <View> 
+        <Text style = {styles.designerName}> Jane Doe</Text>
+        <Text style = {styles.businessName}> Doe Inc. </Text>
       </View>
+
+      <Rating
+        type = 'custom'
+        imageSize = {35}
+        ratingColor = '#3498db'
+        ratingBackgroundColor= 'transparent'
+        readonly
+        startingValue={5}
+        style = {{justifyContent: 'center', paddingLeft: 30}}  />
+    </TouchableOpacity>
+
+    <Divider style = {styles.divider} />
+
+      <TouchableOpacity style = {{flexDirection: 'row'}}> 
+      <Avatar 
+        size = "large"
+        rounded
+        source={{ uri: 'https://s3.amazonaws.com/uifaces/faces/twitter/ladylexy/128.jpg' }} />
+      
+      <View> 
+        <Text style = {styles.designerName}> Jane Doe</Text>
+        <Text style = {styles.businessName}> Doe Inc. </Text>
+      </View>
+
+      <Rating
+        type = 'custom'
+        imageSize = {35}
+        ratingColor = '#3498db'
+        ratingBackgroundColor= 'transparent'
+        readonly
+        startingValue={2}
+        style = {{justifyContent: 'center', paddingLeft: 30}}  />
+    </TouchableOpacity>
+
+    <Divider style = {styles.divider} />
+
+    <TouchableOpacity style = {{flexDirection: 'row'}}> 
+      <Avatar 
+        size = "large"
+        rounded
+        source={{ uri: 'https://s3.amazonaws.com/uifaces/faces/twitter/ladylexy/128.jpg' }} />
+      
+      <View> 
+        <Text style = {styles.designerName}> Jane Doe</Text>
+        <Text style = {styles.businessName}> Doe Inc. </Text>
+      </View>
+
+      <Rating
+        type = 'custom'
+        imageSize = {35}
+        ratingColor = '#3498db'
+        ratingBackgroundColor= 'transparent'
+        readonly
+        startingValue={3}
+        style = {{justifyContent: 'center', paddingLeft: 30}}  />
+    </TouchableOpacity>
+
+    <Divider style = {styles.divider} />
+
+    <TouchableOpacity style = {{flexDirection: 'row'}}> 
+      <Avatar 
+        size = "large"
+        rounded
+        source={{ uri: 'https://s3.amazonaws.com/uifaces/faces/twitter/ladylexy/128.jpg' }} />
+      
+      <View> 
+        <Text style = {styles.designerName}> Jane Doe</Text>
+        <Text style = {styles.businessName}> Doe Inc. </Text>
+      </View>
+
+      <Rating
+        type = 'custom'
+        imageSize = {35}
+        ratingColor = '#3498db'
+        ratingBackgroundColor= 'transparent'
+        readonly
+        startingValue={4}
+        style = {{justifyContent: 'center', paddingLeft: 30}}  />
+    </TouchableOpacity>
+
+    <Divider style = {styles.divider} />
+
+    <TouchableOpacity style = {{flexDirection: 'row'}}> 
+      <Avatar 
+        size = "large"
+        rounded
+        source={{ uri: 'https://s3.amazonaws.com/uifaces/faces/twitter/ladylexy/128.jpg' }} />
+      
+      <View> 
+        <Text style = {styles.designerName}> Jane Doe</Text>
+        <Text style = {styles.businessName}> Doe Inc. </Text>
+      </View>
+
+      <Rating
+        type = 'custom'
+        imageSize = {35}
+        ratingColor = '#3498db'
+        ratingBackgroundColor= 'transparent'
+        readonly
+        startingValue={2}
+        style = {{justifyContent: 'center', paddingLeft: 30}}  />
+    </TouchableOpacity>
+
+    <Divider style = {styles.divider} />
+
+    <TouchableOpacity style = {{flexDirection: 'row'}}> 
+      <Avatar 
+        size = "large"
+        rounded
+        source={{ uri: 'https://s3.amazonaws.com/uifaces/faces/twitter/ladylexy/128.jpg' }} />
+      
+      <View> 
+        <Text style = {styles.designerName}> Jane Doe</Text>
+        <Text style = {styles.businessName}> Doe Inc. </Text>
+      </View>
+
+      <Rating
+        type = 'custom'
+        imageSize = {35}
+        ratingColor = '#3498db'
+        ratingBackgroundColor= 'transparent'
+        readonly
+        startingValue={3}
+        style = {{justifyContent: 'center', paddingLeft: 30}}  />
+    </TouchableOpacity>
+
+    <Divider style = {styles.divider} />
+
+    <TouchableOpacity style = {{alignSelf: 'center'}} onPress={() => navigation.popToTop()}>
+    <Icon
+      reverse
+      name='comment'
+      size = {65}
+      type='ionicon'
+      color='blue' />
+    </TouchableOpacity>
+    
+   
+
+  </View>      
+
     );
   }
 
   const styles = StyleSheet.create({
+    mainview: {
+      flex: 1,
+    },
     findID: {
       fontFamily: 'monospace',
-      fontSize: 20,
+      fontSize: 25,
       textAlign: 'center',
       padding: 20
     },
-    mainView: {
-      flex: 1
+    imagebutton: {
+      borderWidth: 3,
+      borderColor:'rgba(0,0,0,0.2)',
+      width: 90,
+      height: 90,
+      backgroundColor:'#fff',
+      borderRadius: 50,
     },
-    goBack: {
-      backgroundColor: '#ff00bf',
-      padding: 20
-    },
-    goBackText: {
-      fontSize: 15,
-      textAlign: 'center'
-    },
-    goBackContainer: {
+    designers: {
       flex: 1,
-      padding: 20,
-      justifyContent: 'flex-end'
+      justifyContent:'space-around',
+      paddingLeft: 10,
+      backgroundColor: 'white'
+    },
+    divider: {
+      height: 3,
+      backgroundColor: 'black',
+    },
+    designerName: {
+      alignSelf: 'center',
+      fontSize: 20,
+      paddingLeft: 10
+    },
+    businessName: {
+      alignSelf: 'center',
+      fontSize: 20,
+      paddingLeft: 10
     }
+
   });
 
   export default NID;
