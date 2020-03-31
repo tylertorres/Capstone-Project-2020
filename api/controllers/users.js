e04c375ce11cf78e5505f1fec01e7e480f6c1501
Ty <tptorres5@gmail.com>
2020-03-30 21:28:42 -0400

    Fetch all users for home page test completed, now just need to find a way to randomize search of active designers

--- api/controllers/users.js
+++ api/controllers/users.js
@@ -1,12 +1,12 @@
 const express = require('express');
+const User = require('../models/User');
 
 // @info      Load certain amount of designers onto home page of NID
 // @route     GET /api/v1/users
 // @access    Private
-exports.getDesigners = (req, res, next) => {
-  res
-    .status(400)
-    .json({ success: true, msg: 'Acquired designers successfully' });
+exports.getDesigners = async (req, res, next) => {
+  const data = await User.findOne({ email: 'blah@gmail.com' });
+  res.send(data);
 };
 
 exports.getDesigner = (req, res, next) => {
