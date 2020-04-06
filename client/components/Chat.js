8cf59e166d49710f4869ddaef5d9676fe98165ba
Tyler <tptorres5@gmail.com>
2020-04-06 10:35:45 -0400

    Chat works

--- client/components/Chat.js
+++ client/components/Chat.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import { GiftedChat } from 'react-native-gifted-chat'; // 0.3.0
+import { View, Text, StyleSheet, TextInput, TouchableOpacity, Image } from 'react-native';
+import ImagePicker from 'react-native-image-crop-picker';
 
 import Fire from '../firebase/Fire';
 
@@ -13,6 +15,7 @@ class Chat extends Component {
 
   state = {
     messages: [],
+    image: null,
   };
 
   get user() {
@@ -23,21 +26,89 @@ class Chat extends Component {
   }
 
   componentDidMount() {
-    Fire.shared.on((message) =>
+    Fire.shared.on((message) => {
+      //console.log(message);
       // waits for the async operation to complete before persisting new data
       this.setState((previousState) => ({
         messages: GiftedChat.append(previousState.messages, message),
-      }))
-    );
+      }));
+    });
   }
 
   componentWillUnmount() {
     Fire.shared.off();
   }
 
+  setImage = (image) => {
+    this.setState({
+      image: image,
+    });
+  };
+
+  onImageSubmission = () => {
+    ImagePicker.openPicker({}).then((image) => this.setImage(image));
+  };
+
+  onSend = (messages) => {
+    messages.forEach((item) => {
+      const message = {
+        text: item.text,
+        timestamp: Fire.shared.timestamp,
+        user: item.user,
+        image: this.state.image == null ? '' : this.state.image.path,
+        // url passed in by react native image picker
+      };
+      Fire.shared.database.push(message);
+    });
+
+    this.setState({
+      image: null,
+    });
+  };
+
   render() {
-    return <GiftedChat messages={this.state.messages} onSend={Fire.shared.send} user={this.user} />;
+    return (
+      <View style={styles.container}>
+        <View style={styles.upload}>
+          <TouchableOpacity style={styles.button} onPress={this.onImageSubmission}>
+            <Text>Upload</Text>
+          </TouchableOpacity>
+        </View>
+        <GiftedChat
+          messages={this.state.messages}
+          onSend={(messages) => this.onSend(messages)}
+          user={this.user}
+        />
+        {this.state.image && (
+          <View style={styles.preview}>
+            <Image source={{ uri: this.state.image.path, width: 50, height: 50 }} />
+          </View>
+        )}
+      </View>
+    );
   }
 }
+
+const styles = StyleSheet.create({
+  container: { flex: 1 },
+  upload: {
+    display: 'flex',
+    justifyContent: 'center',
+  },
+  button: {
+    alignItems: 'center',
+    backgroundColor: '#DDDDDD',
+    padding: 10,
+  },
+  img: {
+    height: 20,
+    width: 20,
+  },
+  preview: {
+    display: 'flex',
+    justifyContent: 'center',
+    padding: 15,
+  },
+});
 
 export default Chat;
