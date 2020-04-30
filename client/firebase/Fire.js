8b1c73ff3ca8d349e079978e221baa99643a87d1
Tyler <tptorres5@gmail.com>
2020-04-30 13:04:45 -0400

    Merged from test repo (#13)

--- client/firebase/Fire.js
+++ client/firebase/Fire.js
@@ -9,43 +9,47 @@ class Fire {
   init = () => {
     if (!firebase.apps.length) {
       firebase.initializeApp({
-        apiKey: 'AIzaSyAj5mfd9eMZmvcSfZyNav9fjCe1i_v17GM',
-        authDomain: 'indesyn-3fb48.firebaseapp.com',
-        databaseURL: 'https://indesyn-3fb48.firebaseio.com',
-        projectId: 'indesyn-3fb48',
-        storageBucket: 'indesyn-3fb48.appspot.com',
-        messagingSenderId: '929469838203',
-        appId: '1:929469838203:web:68a15f4b6a9aec735a03de',
-        measurementId: 'G-W7VEZCJPC1',
+        apiKey: 'AIzaSyDrEu4rcDoek8RkzeaE_fBPAqRvgrGb_Vo',
+        authDomain: 'indesyndev.firebaseapp.com',
+        databaseURL: 'https://indesyndev.firebaseio.com',
+        projectId: 'indesyndev',
+        storageBucket: 'indesyndev.appspot.com',
+        messagingSenderId: '666199361952',
+        appId: '1:666199361952:web:b230561d22cf65f1fa3cc5',
+        measurementId: 'G-SSMQ0D7F4M',
       });
     }
   };
 
-  observeAuth = () =>
-    firebase.auth().onAuthStateChanged(this.onAuthStateChanged);
-
-  onAuthStateChanged = user => {
-    if (!user) {
-      try {
-        firebase.auth().signInAnonymously();
-      } catch ({ message }) {
-        alert(message);
+  observeAuth = () => {
+    firebase.auth().onAuthStateChanged(user => {
+      if (!user) {
+        try {
+          firebase.auth().signInAnonymously();
+        } catch ({ message }) {
+          alert(message);
+        }
       }
-    }
+    });
   };
 
   get uid() {
     return (firebase.auth().currentUser || {}).uid;
   }
 
-  get database() {
-    // database/messages
-    return firebase.database().ref('messages');
-  }
-
   get timestamp() {
     return firebase.database.ServerValue.TIMESTAMP;
   }
+
+  get database() {
+    return firebase.database().ref('messages');
+  }
+
+  on = callback => this.database.on('child_added', snapshot => callback(this.parse(snapshot)));
+
+  off = () => {
+    this.database.off();
+  };
 
   // parse message and format correctly for Gifted Chat
   parse = message => {
@@ -62,27 +66,20 @@ class Fire {
     };
   };
 
-  on = callback =>
-    this.database.on('child_added', snapshot => callback(this.parse(snapshot)));
-
   // send the message to the Backend, Firebase work
-  send = (messages, image) => {
+  send = messages => {
+    //console.log(messages);
     messages.forEach(item => {
       const message = {
-        text: item.text,
+        text: item.text || '',
         timestamp: firebase.database.ServerValue.TIMESTAMP,
         user: item.user,
-        image: item.image || '',
+        image: item.image.path || '',
         // url passed in by react native image picker
       };
       this.database.push(message);
     });
   };
-
-  // close the connection to the Backend
-  off() {
-    this.database.off();
-  }
 }
 
 Fire.shared = new Fire();
